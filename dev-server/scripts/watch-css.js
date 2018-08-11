e4cdbd33a6cc3eddae41739783377159085431ae
Amin Meyghani <meyghania@gmail.com>
2018-08-11 03:08:39 -0700

    add css source map.

--- dev-server/scripts/watch-css.js
+++ dev-server/scripts/watch-css.js
@@ -6,21 +6,31 @@ const glob = require('glob');
 const CleanCSS = new require('clean-css');
 
 const OUT_FILE = path.join(config.bundleFolder, config.cssBundle);
+const SOURCE_MAP_PATH = `\n/*# sourceMappingURL=${config.cssBundle}.map */`;
+
 const SOURCE = './src/**/*.css';
-const css = new CleanCSS();
+
+const css = new CleanCSS({
+  sourceMap: true,
+  rebaseTo: config.bundleFolder,
+});
 
 var watcher = chokidar.watch('./src/**/*.css', {
   ignored: /(^|[\/\\])\../,
   persistent: true
 })
-.on('all', (event, path) => {
-  console.log('File changed:', event, path);
+.on('all', (event, file) => {
+  console.log('File changed:', event, file);
   glob(SOURCE, (err, files) => {
     css.minify(files, (err, output) => {
       console.log(output.stats);
-      fs.writeFile(OUT_FILE, output.styles, (err) => {
+      fs.writeFile(OUT_FILE, output.styles + SOURCE_MAP_PATH, (err) => {
         if(err) throw new Error('couldnt write the output css.')
-      })
+      });
+
+      fs.writeFile(path.join(config.bundleFolder, 'bundle.css.map'), output.sourceMap.toString(), (err) => {
+        if(err) throw new Error('couldnt write the source map for css.')
+      });
     });
   });
 });
