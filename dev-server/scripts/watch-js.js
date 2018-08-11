5d16ae8cbde07006f72efe23e3539fd2cebfe638
Amin Meyghani <meyghania@gmail.com>
2018-08-11 03:24:37 -0700

    enable source map for js

--- dev-server/scripts/watch-js.js
+++ dev-server/scripts/watch-js.js
@@ -3,7 +3,7 @@ const config = require('../config.json');
 const path = require('path');
 const outputFile = path.join(config.bundleFolder, config.jsBundle);
 
-exec(`npx babel -w src/ --out-file ${outputFile}`, (err, stdout, stderr) => {
+exec(`npx babel -w src/ --out-file ${outputFile} --source-maps`, (err, stdout, stderr) => {
   if (err) {
     throw new Error(err);
   }
